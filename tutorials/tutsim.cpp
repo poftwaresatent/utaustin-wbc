9cc81f6eaa04f7ad843c558980001b0c6f14b1f3
Roland Philippsen <roland dot philippsen at gmx dot net>
2011-04-08 16:57:54 -0700

    draw the tutbot

--- tutorials/tutsim.cpp
+++ tutorials/tutsim.cpp
@@ -31,6 +31,12 @@
 
 #include <jspace/Model.hpp>
 #include <jspace/test/sai_util.hpp>
+#include <tao/dynamics/taoDNode.h>
+
+#include <tao/matrix/TaoDeMath.h>
+#include <tao/matrix/TaoDeQuaternion.h>
+#include <tao/matrix/TaoDeFrame.h>
+
 #include <boost/shared_ptr.hpp>
 
 #include <FL/Fl.H>
@@ -47,60 +53,30 @@
 
 namespace tutsim {
   
-  class Drawing
-    : public Fl_Widget
-  {
+  class Drawing : public Fl_Widget {
   public:
-    Drawing(int xx, int yy, int width, int height, const char * label = 0)
-      : Fl_Widget(xx, yy, width, height, label)
-    {
-    }
+    Drawing(int xx, int yy, int width, int height, const char * label = 0);
     
   protected:
-    virtual void draw()
-    {
-      fl_color(FL_BLACK);
-      fl_rectf(x(), y(), w() - 1, h() - 1);
-      fl_color(FL_WHITE);
-      fl_line(x() + 1, y() + 1, x() + w() - 2, y() + h() - 2);
-      fl_color(FL_YELLOW);
-      fl_line(x() + 1, y() + h() - 2, x() + w() - 2, y() + 1);
-      fl_color(FL_RED);
-      fl_point(x() + w() / 2 - 5, y() + h() / 2 - 5);
-      fl_color(FL_GREEN);
-      fl_point(x() + w() / 2 - 5, y() + h() / 2 + 5);
-      fl_color(FL_BLUE);
-      fl_point(x() + w() / 2 + 5, y() + h() / 2 + 5);
-      fl_color(FL_CYAN);
-      fl_point(x() + w() / 2 + 5, y() + h() / 2 - 5);
-    }
+    virtual void draw();
+    
+    enum {
+      A1, A2, A3, L1, L2, L3, R1, R2, R3, NDOF
+    };
+    
+    taoDNode const * node_[NDOF];
   };
   
   
-  class Window
-    : public Fl_Window
-  {
+  class Window : public Fl_Window {
   public:
-    Window(int width, int height, const char * title)
-      : Fl_Window(width, height, title)
-    {
-      begin();
-      drawing = new Drawing(0, 0, width, height - 50);
-      quit = new Fl_Button(width / 2 - 50, height - 40, 100, 30, "&Quit");
-      quit->callback(cb_quit, this);
-      end();
-      resizable(this);
-      show();
-    }
+    Window(int width, int height, const char * title);
     
     Drawing * drawing;
     Fl_Button * quit;
     
   private:
-    static void cb_quit(Fl_Widget * widget, void * param)
-    {
-      reinterpret_cast<Window*>(param)->hide();
-    }
+    static void cb_quit(Fl_Widget * widget, void * param);
   };
   
 }
@@ -114,11 +90,131 @@ int main(int argc, char ** argv)
 {
   try {
     model.reset(jspace::test::parse_sai_xml_file(model_filename, false));
+    tutsim::Window win(300, 200, "tutsim");
+    return Fl::run();
   }
   catch (std::runtime_error const & ee) {
     errx(EXIT_FAILURE, "EXCEPTION: %s", ee.what());
   }
+}
 
-  tutsim::Window win(300, 200, "tutsim");
-  return Fl::run();
+
+namespace tutsim {
+  
+  Drawing::
+  Drawing(int xx, int yy, int width, int height, const char * label)
+    : Fl_Widget(xx, yy, width, height, label)
+  {
+    node_[0] = 0;
+  }
+  
+  
+  void Drawing::
+  draw()
+  {
+    if ( ! node_[0]) {
+      node_[A1] = model->getNodeByName("a1");
+      node_[A2] = model->getNodeByName("a2");
+      node_[A3] = model->getNodeByName("a3");
+      node_[L1] = model->getNodeByName("l1");
+      node_[L2] = model->getNodeByName("l2");
+      node_[L3] = model->getNodeByName("l3");
+      node_[R1] = model->getNodeByName("r1");
+      node_[R2] = model->getNodeByName("r2");
+      node_[R3] = model->getNodeByName("r3");
+      for (size_t ii(0); ii < NDOF; ++ii) {
+	if ( ! node_[ii]) {
+	  throw std::runtime_error("tutsim::Drawing::draw(): missing node");
+	}
+      }
+    }
+    
+    double scale;
+    if (w() > h()) {
+      scale = h() / 7.06;
+    }
+    else {
+      scale = w() / 7.06;
+    }
+    double const x0(w() / 2.0);
+    double const y0(h() / 2.0);
+    
+    fl_color(FL_BLACK);
+    fl_rectf(x(), y(), w(), h());
+    
+    fl_color(FL_WHITE);
+    fl_line(x0 + (node_[A1]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[A1]->frameGlobal()->translation()[2] * scale),
+	    x0 + (node_[A2]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[A2]->frameGlobal()->translation()[2] * scale));
+    fl_line(x0 + (node_[A2]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[A2]->frameGlobal()->translation()[2] * scale),
+	    x0 + (node_[A3]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[A3]->frameGlobal()->translation()[2] * scale));
+    fl_line(x0 + (node_[L1]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[L1]->frameGlobal()->translation()[2] * scale),
+	    x0 + (node_[R1]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[R1]->frameGlobal()->translation()[2] * scale));
+    
+    fl_color(FL_RED);
+    fl_line(x0 + (node_[R1]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[R1]->frameGlobal()->translation()[2] * scale),
+	    x0 + (node_[R2]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[R2]->frameGlobal()->translation()[2] * scale));
+    fl_line(x0 + (node_[R2]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[R2]->frameGlobal()->translation()[2] * scale),
+	    x0 + (node_[R3]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[R3]->frameGlobal()->translation()[2] * scale));
+    deFrame loc;
+    loc.translation()[1] = -0.15;
+    deFrame aglob;
+    aglob.multiply(*(node_[R3]->frameGlobal()), loc);
+    loc.translation()[1] = 0.15;
+    deFrame bglob;
+    bglob.multiply(*(node_[R3]->frameGlobal()), loc);
+    fl_line(x0 + (aglob.translation()[1] * scale),
+	    y0 - (aglob.translation()[2] * scale),
+	    x0 + (bglob.translation()[1] * scale),
+	    y0 - (bglob.translation()[2] * scale));
+    
+    fl_color(FL_GREEN);
+    fl_line(x0 + (node_[L1]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[L1]->frameGlobal()->translation()[2] * scale),
+	    x0 + (node_[L2]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[L2]->frameGlobal()->translation()[2] * scale));
+    fl_line(x0 + (node_[L2]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[L2]->frameGlobal()->translation()[2] * scale),
+	    x0 + (node_[L3]->frameGlobal()->translation()[1] * scale),
+	    y0 - (node_[L3]->frameGlobal()->translation()[2] * scale));
+    loc.translation()[1] = -0.15;
+    aglob.multiply(*(node_[L3]->frameGlobal()), loc);
+    loc.translation()[1] = 0.15;
+    bglob.multiply(*(node_[L3]->frameGlobal()), loc);
+    fl_line(x0 + (aglob.translation()[1] * scale),
+	    y0 - (aglob.translation()[2] * scale),
+	    x0 + (bglob.translation()[1] * scale),
+	    y0 - (bglob.translation()[2] * scale));
+  }
+  
+  
+  Window::
+  Window(int width, int height, const char * title)
+    : Fl_Window(width, height, title)
+  {
+    begin();
+    drawing = new Drawing(0, 0, width, height - 50);
+    quit = new Fl_Button(width / 2 - 50, height - 40, 100, 30, "&Quit");
+    quit->callback(cb_quit, this);
+    end();
+    resizable(this);
+    show();
+  }
+  
+  
+  void Window::
+  cb_quit(Fl_Widget * widget, void * param)
+  {
+    reinterpret_cast<Window*>(param)->hide();
+  }
+  
 }
