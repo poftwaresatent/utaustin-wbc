0bbe6740ed30275e81c293dceb661853f31347cf
Roland Philippsen <roland dot philippsen at gmx dot net>
2011-04-12 22:46:56 -0700

    tut05_eepos works, and it is pretty cool at that :)

--- tutorials/tut05_eepos.cpp
+++ tutorials/tut05_eepos.cpp
@@ -34,12 +34,14 @@
 #include <opspace/task_library.hpp>
 #include <jspace/test/sai_util.hpp>
 #include <boost/shared_ptr.hpp>
+#include <FL/fl_draw.H>
 #include <err.h>
 
 
 static std::string model_filename(TUTROB_XML_PATH_STR);
 static boost::shared_ptr<jspace::Model> model;
 static boost::shared_ptr<opspace::CartPosTask> task;
+static size_t mode;
 
 
 static bool servo_cb(size_t toggle_count,
@@ -48,7 +50,7 @@ static bool servo_cb(size_t toggle_count,
 		     jspace::State & state,
 		     jspace::Vector & command)
 {
-  size_t const mode(toggle_count % 2);
+  mode = toggle_count % 2;
   static size_t prevmode(42);
   static size_t iteration(0);
   
@@ -81,7 +83,25 @@ static bool servo_cb(size_t toggle_count,
   }
   
   task->update(*model);
-  command = task->getCommand();
+  
+  //////////////////////////////////////////////////
+  // The end-effector position task computes a desired acceleration in
+  // Cartesian space. In order to send it to the joints as desired
+  // torques, this needs to be translated from operational space to
+  // joint space. In the opspace library, this is the job of
+  // opspace::Controller::computeCommand(). However, that needs a bit
+  // of extra infrastructure, so here we simply hardcode the
+  // fundamental equation without any frosting...
+  
+  jspace::Matrix aa;
+  if ( ! model->getMassInertia(aa)) {
+    errx(EXIT_FAILURE, "model->getMassInertia() failed");
+  }
+  jspace::Vector gg;
+  if ( ! model->getGravity(gg)) {
+    errx(EXIT_FAILURE, "model->getGravity() failed");
+  }
+  command = aa * task->getJacobian().transpose() * task->getCommand() + gg;
   
   ++iteration;
   prevmode = mode;
@@ -90,19 +110,58 @@ static bool servo_cb(size_t toggle_count,
 }
 
 
+static void draw_cb(double x0, double y0, double scale)
+{
+  if (0 != mode) {
+    
+    // Here's an example of how to use the parameter reflection aspect
+    // of the opspace library. We get a pointer to the goalpos
+    // parameter, and plot it using fltk's low-level drawing
+    // routines. Here we know to expect a vector parameter, so we use
+    // the more stringent version of lookupParameter().
+    
+    static opspace::Parameter const * goalpos(0);
+    if ( ! goalpos) {
+      goalpos = task->lookupParameter("goalpos", opspace::PARAMETER_TYPE_VECTOR);
+      if ( ! goalpos) {
+	errx(EXIT_FAILURE, "failed to find appropriate goalpos parameter");
+      }
+    }
+    
+    // Remember: we plot the YZ plane, X is sticking out of the screen
+    // but the robot is planar anyway.
+    
+    double const gx(goalpos->getVector()->y());
+    double const gy(goalpos->getVector()->z());
+    fl_color(255, 100, 100);
+    fl_line_style(FL_SOLID, 1, 0);
+    fl_line(x0 + (gx + 0.2) * scale, y0 - gy * scale,
+	    x0 + (gx - 0.2) * scale, y0 - gy * scale);
+    fl_line(x0 + gx * scale, y0 - (gy + 0.2) * scale,
+	    x0 + gx * scale, y0 - (gy - 0.2) * scale);
+    int const rr(ceil(0.15 * scale));
+    int const dd(2 * rr);
+    fl_arc(int(x0 + gx * scale) - rr, int(y0 - gy * scale) - rr, dd, dd, 0.0, 360.0);
+    
+  }
+}
+
+
 int main(int argc, char ** argv)
 {
   try {
     model.reset(jspace::test::parse_sai_xml_file(model_filename, true));
     task.reset(new opspace::CartPosTask("tut05"));
-    jspace::Vector kp(1), kd(1), maxvel(1);
+    jspace::Vector kp(1), kd(1), maxvel(1), ctrlpt(3);
     kp << 100;
     kd << 20;
     maxvel << 1000;
-    task->quickSetup(kp, kd, maxvel, "link4");
+    ctrlpt << 0, 0, -1;
+    task->quickSetup(kp, kd, maxvel, "link4", ctrlpt);
   }
   catch (std::runtime_error const & ee) {
     errx(EXIT_FAILURE, "%s", ee.what());
   }
+  tutsim::set_draw_cb(draw_cb);
   return tutsim::run(servo_cb);
 }
